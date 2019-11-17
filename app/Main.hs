c402b417b63b1a44ae87fb3b7e5ed7cdbe6a27cc
Carlo Keizer <ck.soest@hotmail.com>
2019-11-17 21:46:50 +0100

    second try
    second try
    add documentation

--- app/Main.hs
+++ app/Main.hs
@@ -2,26 +2,29 @@ module Main where
 
 import GameOfLife
 
+-- | Deze functie stelt een aantal vragen aan de gebruiker die nodig zijn 
 main :: IO ()
 main = do 
-         putStr "Give size height grid: "
+         putStrLn "Give height grid (number e.g. 10): "
          y <- getLine
-         putStr "Give size weight grid: "
+         putStrLn "Give weight grid (number e.g. 10): "
          x <- getLine
-         let h = (read y :: Int)
-         let w = (read x :: Int)
-         putStr "Give begin state: " -- possoble option = [(10,10),(10,11),(10,12),(9,12),(8,11)]
+         let h = read y :: Int
+         let w = read x :: Int
+         putStrLn "Give begin living cells grid (Coords e.g. [(5,5),(5,6),(5,7),(6,5),(6,7),(7,5),(7,6),(7,7)]) : "
          bg <- getLine
-         let beginState = (read bg :: Coords)
-         let grid = buildGrid h w
-         let beginGrid = setStates beginState Alive grid
-         print(beginGrid)
-         printNextGen beginGrid
+         let beginState = read bg :: Coords
+         let grid = buildGrid h w beginState
+         print grid
+         printNextGen beginState h w
          
-
-printNextGen :: Grid -> IO ()
-printNextGen grid = do
-                     putStr "Press enter for next generation: "
+-- | Deze functie laat een grid zien op het scherm waarbij aangeven is welke cellen levend moeten zijn en hoe groot het grid moet zijn.
+printNextGen :: Coords -- ^ De coördinaten de levend in het grid moet zijn.
+             -> Int -- ^ De hoogte die het grid moet hebben.
+             -> Int -- ^ De breedte die het grid moet hebben.
+             -> IO () 
+printNextGen coords h w = do
+                     putStrLn "Press enter for next generation: "
                      n <- getLine
-                     if n == "" then print(nextGen grid) else printNextGen grid
-                     printNextGen (nextGen grid)
+                     if n == "" then print(buildGrid h w (nextGen coords)) else printNextGen coords h w
+                     printNextGen (nextGen coords) h w
--- src/GameOfLife.hs
+++ src/GameOfLife.hs
@@ -1,56 +1,86 @@
 module GameOfLife where
 
-import qualified Data.Map.Lazy as Map
+import Data.List (nub)
 
-data State = Alive | Dead deriving (Eq)
-type States = [State]
+type Cells = [Cell]
+type Cell = (Coord, State)
 type Coord = (Int, Int)
 type Coords = [Coord]
-data Grid = Grid Int Int (Map.Map Coord State)
+data State = Alive | Dead deriving (Eq)
+type States = [State]
+data Grid = Grid Int Int Cells
 
 instance Show Grid where
-    show(Grid h w x) = if (Map.size x) == 0 then "" else show(Map.elems (Map.take w x)) ++ "\n" ++ show(Grid h w (Map.drop w x))
+    show(Grid h w []) = ""
+    show(Grid h w x) = let (_, states) = unzip x
+                       in show(take w states) ++ "\n" ++ show(Grid h w (drop w x))
 
 instance Show State where
-    show(Alive) = "@"
-    show(Dead) = " "
+    show Alive = "#"
+    show Dead = " "
 
-buildGrid :: Int -> Int -> Grid
-buildGrid h w = (Grid h w (Map.fromList [((i,j), Dead) | i <- [1..h], j <- [1..w]]))
 
-setStates :: Coords -> State -> Grid -> Grid
-setStates [] _ grid = grid
-setStates (x:xs) state grid = setStates xs state (setState x state grid)
+-- | Deze functie berekent de nieuwe generatie van een grid op basis van een gegeven lijst met levende coördinaten.
+nextGen :: Coords -- ^ Een lijst met levende coördinaten als oude generatie.
+        -> Coords -- ^ Een lijst met levende coördinaten als nieuwe generatie.
+nextGen coords = let cellCoords = nub (concatMap getNeighbours coords)
+                     newCellStates = customMap nextGenCell cellCoords coords
+                 in takeAliveCells (zip cellCoords newCellStates)
 
-setState :: Coord -> State -> Grid -> Grid
-setState coord state (Grid h w x) = (Grid h w (Map.insert coord state x))
+-- | Deze functie berekent de nieuwe generatie van een coördinaat.
+nextGenCell :: Coord -- ^ De coördinaat waarvan de nieuwe generatie berekent moet worden.
+            -> Coords -- ^ Een lijst met cecoördinaten waarvan we weten dat ze levend zijn.
+            -> State -- ^ De nieuwe staat die de gegeven cel moet krijgen in de nieuwe generatie.
+nextGenCell coord coords = checkRules (customMap checkState (getNeighbours coord) coords)
 
-getState :: Coord -> Grid -> State
-getState (a,b) (Grid h w x) = if a < 1 || a > h || b < 1 || b > w then Dead else x Map.! (a,b)
+-- | Deze functie berekent de coördinaten van de buren van een gegeven cel.
+getNeighbours :: Coord -- ^ De coördinaat waarvan de buren berekent moeten worden.
+              -> Coords -- ^ De coördinaten van de buren van de gegeven coördinaat.
+getNeighbours (y,x) = [(y, x), (y-1, x-1), (y-1, x), (y-1, x+1), (y, x-1), (y, x+1), (y+1, x-1), (y+1, x), (y+1, x+1)]
 
-getStates :: Coords -> Grid -> States
-getStates coords grid = customMap getState coords grid
+-- | Deze functie berekent of een coördinaat leven of dood is.
+checkState :: Coord -- ^ De coördinaat waarvan je wilt weten welke staat hij heeft
+           -> Coords -- ^ De coördinaten waarvan je weet de ze levend zijn.
+           -> State -- ^ De berekende staat van de gegeven coördinaat.
+checkState coord coords = if coord `elem` coords then Alive else Dead
 
-getNeighbours :: Coord -> Coords
-getNeighbours (a,b) = [(a, b), (a-1, b-1), (a-1, b), (a-1, b+1), (a, b-1), (a, b+1), (a+1, b-1), (a+1, b), (a+1, b+1)]
-
-checkRules :: States -> State
+-- | Deze functie berekent wat de nieuwe staat van een cel is aan de hand van een lijst met staten.
+checkRules :: States -- ^ Een lijst met 9 staten waarvan de eerste staat in de lijst de oude staat is en de rest de buren zijn van de cell..
+           -> State -- ^ De nieuw berekende staat aan de hand van de buren van de cel
 checkRules states
-                | head states == Alive && (length $ filter (==Alive) $ tail states) < 2 = Dead
-                | head states == Alive && (length $ filter (==Alive) $ tail states) == 2 = Alive
-                | head states == Alive && (length $ filter (==Alive) $ tail states) == 3 = Alive
-                | head states == Alive && (length $ filter (==Alive) $ tail states) > 3 = Dead
-                | head states == Dead && (length $ filter (==Alive) $ tail states) == 3 = Alive
+                | head states == Alive && length (filter (==Alive) $ tail states) == 2 = Alive
+                | head states == Alive && length (filter (==Alive) $ tail states) == 3 = Alive
+                | head states == Dead && length (filter (==Alive) $ tail states) == 3 = Alive
                 | otherwise = Dead
 
-nextGen :: Grid -> Grid
-nextGen (Grid h w x) = let keys = (Map.keys x)
-                           neighbours = map getNeighbours keys
-                           states = customMap getStates neighbours (Grid h w x)
-                           newStates = map checkRules states
-                           newGrid = Map.fromList (zip keys newStates)
-                       in (Grid h w newGrid)
+-- | Deze functie berekent de welke cellen levend zijn in een lijst van cellen.
+takeAliveCells :: Cells -- ^ Een lijst met cellen waarvan berekent word welke levend zijn.
+               -> Coords -- ^ Een lijst met coördinaten die de staat levend hebben.
+takeAliveCells [] = []
+takeAliveCells ((coord,state):xs) = if state == Alive then coord : takeAliveCells xs else takeAliveCells xs
 
-customMap :: (a -> Grid -> b) -> [a] -> Grid -> [b]
-customMap f [] grid = []
-customMap f (x:sx) grid = (f x grid):customMap f sx grid
+-- | Deze functie bouwt een grid waarbij je kan aangeven hoe groot hij moet zijn, en welke cellen levend moeten zijn.
+buildGrid :: Int -- ^ Een getal die de grootte van het grid aangeeft in de hoogte.
+          -> Int -- ^ Een getal die de grootte van het grid aangeeft in de breedte.
+          -> Coords -- ^ Een lijst met coördinaten die levend gemaakt worden in het grid.
+          -> Grid -- ^ Het gecreeërde grid met de aangegeven grootte en welke cellen levend zijn.
+buildGrid h w coords = setStatesAlive coords (Grid h w [((y,x), Dead) | y <- [1..h], x <- [1..w]])
+
+-- | Deze functie maakt cellen in een grid levend aan de hand van een lijst met coördinaten.
+setStatesAlive :: Coords -- ^ Een lijst met coördinaten die levend gemaakt moeten worden in het grid
+               -> Grid -- ^ Het gegeven grid waar cellen levend in gemaakt moeten worden.
+               -> Grid -- ^ Het grid waar de aangegeven cellen levend zijn gemaakt.
+setStatesAlive coords grid = foldl (flip setStateAlive) grid coords
+
+-- | Deze functie maakt een aangegeven cell levend in een grid.
+setStateAlive :: Coord -- ^ De coördinaat die levend gemaakt moet worden in het gegeven grid.
+              -> Grid -- ^ Het gegeven grid waar de cel levend in gemaakt moet worden.
+              -> Grid -- ^ Het grid waar de aangegeven cel levend gemaakt is.
+setStateAlive coordA (Grid h w x) = Grid h w (map (\(coordB, state) -> if coordB == coordA then (coordB, Alive) else (coordB, state)) x)
+
+-- | Dit is een geabstraheerde functie die bijna gelijk is aan de functie map. Het ver schil is dat je bij deze functie een extra argument kan meegeven aan de functie die je wilt mappen over de lijst. 
+customMap :: (a -> b -> c) -- ^ De functie die gemapt word over de lijst.
+          -> [a] -- ^ De lijst waarover de functie gemapt word.
+          -> b -- ^ Het extra argument die nodig is voor de functie.
+          -> [c] -- ^ De lijst waarover gemapt is met de functie met het extra argument.
+customMap f a b = map (`f` b) a
